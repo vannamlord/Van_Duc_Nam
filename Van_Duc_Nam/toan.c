a29fb4349243ae9482aab997c99b2657f173cc2e
vannamlord <vdnam.ute@gmail.com>
2025-03-15 16:18:54 +0700

    update

---
+++ Van_Duc_Nam/toan.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int Cong(int a, int b) {
+	int kq = 0;
+	kq = a + b;
+	return kq;
+}
+
+int tru(int a, int b) {
+	int kq = 0;
+	kq = a - b;
+	return kq;
+}
+
+int nhan(int a, int b) {
+	int kq = 0;
+	kq = a * b;
+	return kq;
+}
+
+float chia(int a, int b) {
+	int kq = 0;
+	kq = a / b;
+	return kq;
+}
