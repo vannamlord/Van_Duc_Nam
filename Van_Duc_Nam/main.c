fed7f43a5a46d64d9ff29142d8bc6736be9b238f
vannamlord <vdnam.ute@gmail.com>
2025-03-16 09:42:53 +0700

    update ky tu viet hoa

--- Van_Duc_Nam/main.c
+++ Van_Duc_Nam/main.c
@@ -2,9 +2,12 @@
 
 void main(){
 	int tongso = 0;
-	char str[] = "hello";
-	while (str[tongso] != 0) {
+	char str[] = "heLLo";
+	while (str[tongso] !=0) {
+		if (str[tongso] < 90) {
+			printf("Ky tu la chu viet hoa tai vi tri: %d ky tu\n", tongso);
+		}
 		tongso += 1;
 	}
-	printf("Chuoi co do dai: %d ky tu\n", tongso);
+	
 }
