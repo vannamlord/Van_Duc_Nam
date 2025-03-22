42e1399d4dc005f0fb2fa4133179635acad187be
vannamlord <vdnam.ute@gmail.com>
2025-03-22 23:41:15 +0700

    update bai 2

--- Van_Duc_Nam/main.c
+++ Van_Duc_Nam/main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
 int demkytu(char* chuoi);
-
+int demkhoangtrang(char* chuoi);
 void main(){
 	char* chuoi = "Hello World";
 	int sokytu = demkytu(chuoi);
-	printf("Bai 1: So ky tu trong chuoi: %d", sokytu);
+	printf("Bai 1: So ky tu trong chuoi: %d\n", sokytu);
+
+	char* chuoi_2 = "Hello World new world data nag";
+	int tongspace = demkhoangtrang(chuoi_2);
+	printf("Bai 2: So luong space: %d\n", tongspace);
+
+
 }
--- Van_Duc_Nam/stringfunction.c
+++ Van_Duc_Nam/stringfunction.c
@@ -7,3 +7,15 @@ int demkytu(char* chuoi) {
 	}
 	return i;
 }
+
+int demkhoangtrang(char* chuoi) {
+	int i = 0;
+	int tong = 0;
+	while (chuoi[i] != 0) {
+		i++;
+		if (chuoi[i] == ' ') {
+			tong += 1;
+		}
+	}
+	return tong;
+}
