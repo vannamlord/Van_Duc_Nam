e587c7553156b7c0bde5e044a6c3e1e74c363785
vannamlord <vdnam.ute@gmail.com>
2025-03-22 23:52:24 +0700

    update bai 3

--- Van_Duc_Nam/main.c
+++ Van_Duc_Nam/main.c
@@ -2,14 +2,27 @@
 
 int demkytu(char* chuoi);
 int demkhoangtrang(char* chuoi);
+void uppercase(char* chuoi) {
+	int i = 0;
+	while (chuoi[i] != 0) {
+		if ((chuoi[i] > 96) && (chuoi[i] <123)) {
+			chuoi[i] = chuoi[i] - 32;
+		}
+		i++;
+	}
+	printf("Bai 3: Chuoi in hoa: %s\n", chuoi);
+}
 void main(){
-	char* chuoi = "Hello World";
+	char chuoi[] = "Hello World";
 	int sokytu = demkytu(chuoi);
 	printf("Bai 1: So ky tu trong chuoi: %d\n", sokytu);
 
-	char* chuoi_2 = "Hello World new world data nag";
+	char chuoi_2[] = "Hello World new world data nag";
 	int tongspace = demkhoangtrang(chuoi_2);
 	printf("Bai 2: So luong space: %d\n", tongspace);
+
+	char chuoi_3[] = "Hello World new world data nag";
+	uppercase(chuoi_3);
 
 
 }
