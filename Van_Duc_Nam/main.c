d120319ba9d53384efe14f6ad5b52b88357e2e12
vannamlord <vdnam.ute@gmail.com>
2025-04-13 00:17:50 +0700

    Bai 5: struct rut gon phan so

--- Van_Duc_Nam/main.c
+++ Van_Duc_Nam/main.c
@@ -10,6 +10,7 @@ phan_so_t nhan_x(phan_so_t A, phan_so_t B);
 phan_so_t chia_x(phan_so_t A, phan_so_t B);
 phan_so_t cong_x(phan_so_t A, phan_so_t B);
 phan_so_t tru_x(phan_so_t A, phan_so_t B);
+phan_so_t rutgon_x(phan_so_t A);
 void main(){
 	phan_so_t A;
 	phan_so_t B;
@@ -20,8 +21,14 @@ void main(){
 	B.tu = 3;
 	B.mau = 2;
 
-	phan_so_t data = tru_x(A, B);
+	phan_so_t data = nhan_x(A, B);
+	phan_so_t rt = rutgon_x(data);
+	if (rt.mau == 1) {
+		printf("rut gon %d\n", rt.tu);
+	}
+	else {
+		printf("rut gon tu %d\n", rt.tu);
+		printf("rut gon mau %d\n", rt.mau);
+	}
 	
-	printf("tu tru %d\n", data.tu);
-	printf("mau tru %d\n", data.mau);
 }
--- Van_Duc_Nam/structfunction.c
+++ Van_Duc_Nam/structfunction.c
@@ -32,3 +32,47 @@ phan_so_t tru_x(phan_so_t A, phan_so_t B) {
 	x.mau = A.mau * B.mau;
 	return x;
 }
+
+phan_so_t rutgon_x(phan_so_t A) {
+	phan_so_t x;
+	x.tu = A.tu;
+	x.mau = A.mau;
+	int nego = 0;
+	if (A.tu < 0) {
+		A.tu = -A.tu;
+		nego = 1;
+	}
+	if (A.tu > A.mau) {
+		if (A.tu % A.mau == 0) {
+			x.tu = A.tu / A.mau;
+			x.mau = 1;
+		}
+		else {
+			for (int i = 2; i < A.mau; i++) {
+				if((A.tu % i ==0)&&(A.mau%i==0)){
+					x.tu = A.tu / i;
+					x.mau = A.mau / i;
+				}
+			}
+		}
+	}
+	else {
+		if (A.mau % A.tu == 0) {
+			x.tu = 1;
+			x.mau = A.mau / A.tu;
+		}
+		else
+		{
+			for (int i = 2; i < A.tu; i++) {
+				if ((A.tu % i == 0) && (A.mau % i == 0)) {
+					x.tu = A.tu / i;
+					x.mau = A.mau / i;
+				}
+			}
+		}
+	}
+	if (nego == 1) {
+		x.tu = -x.tu;
+	}
+	return x;
+}
