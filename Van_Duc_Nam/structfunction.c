07de8f3a179c364df22b0f6b0b0d25c2aaaaceba
vannamlord <vdnam.ute@gmail.com>
2025-04-20 00:46:52 +0700

    update

--- Van_Duc_Nam/structfunction.c
+++ Van_Duc_Nam/structfunction.c
@@ -2,6 +2,47 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <stdint.h>
+
+typedef struct {
+	char ten[30];
+	int tuoi;
+	int gioitinh;
+	float diemtoan;
+	float diemvan;
+	float diemtb;
+	int xeploai;
+} hoc_sinh;
+
+hoc_sinh nhapthongtin() {
+	hoc_sinh kq = { 0 };
+	printf("Nhap ten:");
+	scanf("%[^\n]", &kq.ten);
+	printf("Nhap tuoi:");
+	scanf("%d", &kq.tuoi);
+	printf("Nhap gioi tinh:");
+	scanf("%d", &kq.gioitinh);
+	printf("Nhap diem toan:");
+	scanf("%f", &kq.diemtoan);
+	printf("Nhap diem van:");
+	scanf("%f", &kq.diemvan);
+	kq.diemtb = (kq.diemtoan + kq.diemvan) / 2;
+	getc(stdin);
+	if (kq.diemtb >= 8) {
+		kq.xeploai = 0;
+	}
+	else if (kq.diemtb >= 6.5) {
+		kq.xeploai = 1;
+	}
+	else if (kq.diemtb >= 5.0) {
+		kq.xeploai = 2;
+	}
+	else
+	{
+		kq.xeploai = 3;
+	}
+	return kq;
+}
+
 typedef struct {
 	int tu;
 	int mau;
@@ -77,4 +118,43 @@ phan_so_t rutgon_x(phan_so_t A) {
 		x.tu = -x.tu;
 	}
 	return x;
+}
+
+void print_danhsach(hoc_sinh* danhsach, int n) {
+	for (int i = 0; i < n; i++) {
+		printf("Ten hs_%d: %s\n", i, danhsach[i].ten);
+	}
+}
+void max_diemtb(hoc_sinh* danhsach, int n) {
+	float max = 0;
+	int vitrihs = n;
+	for (int i = 0; i < n; i++) {
+		if (danhsach[i].diemtb > max) {
+			max = danhsach[i].diemtb;
+			vitrihs = i;
+		}
+	}
+	if (vitrihs != n) {
+		printf("Ten hs: %s voi diemtb: %f\n", danhsach[vitrihs].ten, max);
+	}
+	else
+	{
+		printf("Khong co danh sach");
+	}
+}
+
+void sapxep_ds(hoc_sinh* danhsach, int n) {
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++)
+		{
+			if (danhsach[i].diemtb < danhsach[j].diemtb) {
+				hoc_sinh tam = danhsach[i];
+				danhsach[i] = danhsach[j];
+				danhsach[j] = tam;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		printf("Danh sach ten: %s\n", danhsach[i].ten);
+	}
 }
